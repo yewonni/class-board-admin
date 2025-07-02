bf0bba5dc588bafb50fd80c3e1305ce24f28bda4
ddhsl <dnjs9199@naver.com>
2025-07-02 15:57:09 +0900

    [add] TopCoursesSection 컴포넌트 추가

---
+++ client/src/components/dashboard/TopCoursesSection.tsx
@@ -0,0 +1,8 @@
+export default function TopCoursesSection() {
+  return (
+    <section className="bg-white w-full rounded-md p-6 shadow-sm">
+      <h2 className="font-bold text-lg mb-2">인기 강의 TOP 5</h2>
+      <div className="w-full h-[220px] border border-primary">표 삽입</div>
+    </section>
+  );
+}
