bda26dd65d08cb56a574880972b5817326fa8cf3
ddhsl <dnjs9199@naver.com>
2025-07-02 15:56:06 +0900

    [add] NewStudentsByMonthSection 컴포넌트 추가

---
+++ client/src/components/dashboard/NewStudentsByMonthSection.tsx
@@ -0,0 +1,8 @@
+export default function NewStudentsByMonthSection() {
+  return (
+    <section className="bg-white w-full rounded-md p-6 shadow-sm">
+      <h2 className="font-bold text-lg mb-2">월별 신규 수강생 수 추이</h2>
+      <div className="w-full h-[220px] border border-primary">차트삽입</div>
+    </section>
+  );
+}
