9faefdb93fbe9af05bb2ad3c8f53402f8013ee7e
ddhsl <dnjs9199@naver.com>
2025-07-02 15:55:46 +0900

    [add] AttendanceRageByCourseSection 컴포넌트 추가

---
+++ client/src/components/dashboard/AttendanceRateByCourseSection.tsx
@@ -0,0 +1,8 @@
+export default function AttendanceRateByCourseSection() {
+  return (
+    <section className="bg-white w-full rounded-md p-6 shadow-sm">
+      <h2 className="font-bold text-lg mb-2">강의별 출석률 비교</h2>
+      <div className="w-full h-[220px] border border-primary">차트삽입</div>
+    </section>
+  );
+}
