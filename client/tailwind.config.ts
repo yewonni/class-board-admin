bbed78ff84a96ef5828e8df4324a60fd528b0513
ddhsl <dnjs9199@naver.com>
2025-07-02 15:49:38 +0900

    [add] tailwind.config: color, backgroundImage, font 추가

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -7,8 +7,25 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#96B956",
+        primaryHover: "#B7D46B",
+        primaryActive: "#769B24",
+        secondary: "#CBD5E1",
+        sidebarBg: "#111827",
+        mainBg: "#F4F4F6",
+        accent: "#4B8C79",
+      },
+      backgroundImage: {
+        "search-icon": "url('/images/search-icon.svg')",
+      },
+      fontFamily: {
+        sans: ["var(--font-inter)", "ui-sans-serif", "system-ui", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
+
 export default config;
