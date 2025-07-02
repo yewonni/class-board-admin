431a139c58fb1c28f503e58149332033fa621b11
ddhsl <dnjs9199@naver.com>
2025-07-02 15:49:59 +0900

    [add] axiosInstance 추가

---
+++ client/src/api/axiosInstance.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default axiosInstance;
