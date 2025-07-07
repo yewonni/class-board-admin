c9e64dc7f8ed0dc617322c0465223d0ecf9dc561
ddhsl <dnjs9199@naver.com>
2025-07-07 21:21:17 +0900

    [add] courses 조회 및 수정 함수 추가

---
+++ client/src/api/courses/courses.ts
@@ -0,0 +1,63 @@
+import axiosInstance from "../axiosInstance";
+
+interface LecturesResponse {
+  data: {
+    id: number;
+    title: string;
+    instructor: string;
+    openDate: string;
+    status: string;
+    studentCount: number;
+  }[];
+  pagination?: {
+    page: number;
+    limit: number;
+    totalCount: number;
+  };
+  totalLecturesCount: number;
+}
+
+export const getLectures = (
+  page = 0,
+  limit = 20,
+  status?: string,
+  search?: string
+) => {
+  return axiosInstance.get<LecturesResponse>("/courses", {
+    params: {
+      page,
+      limit,
+      status,
+      search,
+    },
+  });
+};
+
+interface Courses {
+  id: number;
+  name: string;
+  email: string;
+}
+
+interface CoursesDetail {
+  id: number;
+  title: string;
+  instructor: string;
+  openDate: string;
+  status: string;
+  studentCount: number;
+  progress: string;
+  students: Courses[];
+}
+
+export const getCourseById = (id: number) => {
+  return axiosInstance.get<CoursesDetail>(`/courses/${id}`);
+};
+
+interface CourseStatusRequest {
+  id: number;
+  status: string;
+}
+export const editCourseStatus = ({ id, status }: CourseStatusRequest) => {
+  return axiosInstance.put(`/courses/${id}`, { status });
+};
