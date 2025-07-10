9f330cb4d8becd337c2ec817628f4f2cfaaf56e3
ddhsl <dnjs9199@naver.com>
2025-07-10 23:55:42 +0900

    [edit] SearchBar : value, onChange props 추가

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,11 +1,20 @@
 interface SearchBarPropsType {
   placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
-export default function SearchBar({ placeholder }: SearchBarPropsType) {
+
+export function SearchBar({
+  placeholder,
+  value,
+  onChange,
+}: SearchBarPropsType) {
   return (
     <input
       type="text"
       placeholder={placeholder}
+      value={value}
+      onChange={onChange}
       className="w-[300px] p-2  rounded-md text-sm border border-secondary bg-search-icon bg-no-repeat focus:outline-none focus:ring-2 focus:ring-primary"
       style={{ backgroundPosition: "right 12px center" }}
     />
