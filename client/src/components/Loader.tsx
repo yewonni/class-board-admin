c14a24b62377a8833552607685d183c972d8667e
ddhsl <dnjs9199@naver.com>
2025-07-10 23:54:49 +0900

    [add] Loader 컴포넌트 추가

---
+++ client/src/components/Loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="w-full flex justify-center items-center py-10">
+      <div className="w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full animate-spin" />
+    </div>
+  );
+}
