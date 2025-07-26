5a0ad880247328d8246186fe0b889b248fda0cdb
ddhsl <dnjs9199@naver.com>
2025-07-26 18:24:43 +0900

    [edit] next.config option 추가

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  compress: true,
+
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
+
+  experimental: {
+    scrollRestoration: true,
+  },
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias["@sentry/node"] = "@sentry/browser";
+    }
+
+    return config;
+  },
 };
 
-export default nextConfig;
+export default bundleAnalyzer(nextConfig);
