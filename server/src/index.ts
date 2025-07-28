07ae11d47cda1ebed449083a4293e5a81b863e54
ddhsl <dnjs9199@naver.com>
2025-07-28 18:33:54 +0900

    [edit] server- index : env 분기 처리 로직 추가

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,13 @@
+import dotenv from "dotenv";
+import path from "path";
+
+const envPath =
+  process.env.NODE_ENV === "production"
+    ? path.resolve(__dirname, "../.env.production")
+    : path.resolve(__dirname, "../.env.local");
+
+dotenv.config({ path: envPath });
+
 import app from "./app";
 
 const PORT = process.env.PORT || 4000;
